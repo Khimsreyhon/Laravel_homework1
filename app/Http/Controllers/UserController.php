3fa41e796d093bbcde96d1dcc8add5d818ca7804
Khimsreyhon <sreyhon.khim@student.passerellesnumeriques.org>
2021-10-26 17:12:36 +0700

    commit message

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\
+class UserController extends Controller
+{
+    //
+    public function register(Request $request){
+
+    }
+    public function login(Request $request){
+
+    }
+    public function loout(Request $request){
+
+    }
+}
